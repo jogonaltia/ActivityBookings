7aa8ed49b12c796c2e7d498ddf9aae14b094159b
jogonaltia <jonatan.gonzalez@altia.es>
2024-04-23 17:08:08 +0200

    Creacion y uso de pipe activityTitle

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,22 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Activity } from '../domain/activity.type';
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { ActivityTitlePipe } from "./activity-title.pipe";
 
 @Component({
-  selector: 'lab-bookings',
-  standalone: true,
-  imports: [CurrencyPipe, DatePipe, UpperCasePipe],
-  template: `
-    <p>{{ activity.name }}</p>
-    <div>
-      <span>{{ activity.location }}</span>
-      <span>{{ activity.price | currency }}</span>
-      <span>{{ activity.date | date:'dd-MMM-yyyy' }}</span>
-      <span>{{ activity.status | uppercase }}</span>
-    </div>
+    selector: 'lab-bookings',
+    standalone: true,
+    imports: [CurrencyPipe, DatePipe, UpperCasePipe, ActivityTitlePipe],
+    template: `
+    <article>
+      <header>
+        <h2>{{ activity | activityTitle }}</h2>
+        <div [class]="activity.status">
+          <span>{{ activity.location }}</span>
+          <span>{{ activity.price | currency }}</span>
+          <span>{{ activity.date | date:'dd-MMM-yyyy' }}</span>
+          <span>{{ activity.status | uppercase }}</span>
+        </div>
+      </header>
+      <footer>
+        <button>Book now!</button>
+      </footer>
   `,
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+    styles: ``,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookingsComponent {
   activity: Activity = {
