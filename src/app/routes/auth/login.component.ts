ba9f98ad83dcc121426e0fc6f6ee2a295c7dd204
jogonaltia <jonatan.gonzalez@altia.es>
2024-04-28 12:10:21 +0200

    Se agregan formularios a las páginas de login y registro

--- src/app/routes/auth/login.component.ts
+++ src/app/routes/auth/login.component.ts
@@ -5,8 +5,27 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   template: `
-    <p>login works!</p>
-    <a routerLink="/auth/register">Register</a>
+    <article>
+      <header>
+        <h2>Login</h2>
+      </header>
+      <main>
+        <form>
+          <label for="email">
+            <span>Email</span>
+            <input id="email" type="email" />
+          </label>
+          <label for="password">
+            <span>Password</span>
+            <input id="password" type="password" />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </main>
+      <footer>
+        <a [routerLink]="['/auth', 'register']">Register if don't have an account</a>
+      </footer>
+    </article>
   `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush
