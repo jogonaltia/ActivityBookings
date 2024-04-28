848912ef2debb3c79cb465d751382789fa0884c6
jogonaltia <jonatan.gonzalez@altia.es>
2024-04-28 16:07:34 +0200

    Agregado parametro en la ruta 'bookins/:slug' para el envío y la recepción. Refactorización de 'bookings.page.ts'

--- src/app/routes/bookings/bookings.page.ts
+++ src/app/routes/bookings/bookings.page.ts
@@ -1,135 +1,130 @@
-import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
-import { Activity } from '../../domain/activity.type';
+import { Component, computed, effect, input, signal } from '@angular/core';
+import { NULL_ACTIVITY } from '../../domain/activity.type';
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
-import { ActivityTitlePipe } from "./activity-title.pipe";
 import { FormsModule } from '@angular/forms';
+import { ACTIVITIES } from '../../domain/activities.data';
 
 @Component({
-    standalone: true,
-    imports: [CurrencyPipe, DatePipe, UpperCasePipe, ActivityTitlePipe, FormsModule],
-    template: `
-    <article>
-      <header>
-        <h2>{{ activity | activityTitle }}</h2>
-        <div [class]="activity.status">
-          <span>{{ activity.location }}</span>
-          <span>{{ activity.price | currency }}</span>
-          <span>{{ activity.date | date:'dd-MMM-yyyy' }}</span>
-          <span>{{ activity.status | uppercase }}</span>
-        </div>
-      </header>
-      <main>
-        <h4>Participants</h4>
-        <div>Already Participants: {{ alreadyParticipants }}</div>
-        <div>
-          @for(participant of participants(); track participant.id) {
-            <span>{{ participant.id }}</span>
-          } @empty {
-            <span>No participants yet</span>
+  standalone: true,
+  imports: [CurrencyPipe, DatePipe, UpperCasePipe, FormsModule],
+  template: `
+    @if(activity(); as activity) {
+      <article>
+        <header>
+          <h2>{{ activity.name }}</h2>
+          <div [class]="activity.status">
+            <span>{{ activity.location }}</span>
+            <span>{{ activity.price | currency }}</span>
+            <span>{{ activity.date | date:'dd-MMM-yyyy' }}</span>
+            <span>{{ activity.status | uppercase }}</span>
+          </div>
+        </header>
+        <main>
+          <h4>Participants</h4>
+          <div>Already Participants: {{ alreadyParticipants() }}</div>
+          <div>Max Participants: {{ activity.maxParticipants }}</div>
+          <ul>
+            <li>New Participants: {{ newParticipants() }}</li>
+            <li>Total Participants: {{ totalParticipants() }}</li>
+            <li>Remaining places: {{ remainingPlaces() }} </li>
+          </ul>
+          <div>
+            @for(participant of participants(); track participant.id) {
+              <span>{{ participant.id }}</span>
+            } @empty {
+              <span>No participants yet</span>
+            }
+          </div>
+        </main>
+        <footer>
+          @if (isBookable()) {
+            <h4>New Bookings</h4>
+              @if(remainingPlaces() > 0) {
+                <label for="newParticipants">How many participants want a book?</label>
+                <input
+                  type="number"
+                  [ngModel]="newParticipants()"
+                  (ngModelChange)="onNewParticipantsChange($event)"
+                  min="0"
+                  [max]="maxParticipants()"
+                />
+              } @else {
+                <div>
+                  <button class="secondary outline" (click)="onNewParticipantsChange(0)">
+                    Reset
+                  </button>
+                  <span>No more places availables</span>
+                </div>
+              }
+              <button [disabled]="booked() || newParticipants() === 0" (click)="onBookClick()">
+                Book {{ newParticipants() }} places now for {{ bookingAmount() | currency }}!
+              </button>
+              <div>{{ bookedMessage() }}</div>
           }
-        </div>
-        <ul>
-          <li>New Participants: {{ newParticipants() }}</li>
-          <li>Total Participants: {{ totalParticipants() }}</li>
-          <li>Remaining places: {{ remainingPlaces() }} </li>
-        </ul>
-      </main>
-      <footer>
-        <h4>New Bookings</h4>
-        @if(remainingPlaces() > 0) {
-          <label for="newParticipants">How many participants want a book?</label>
-          <input
-            type="number"
-            [ngModel]="newParticipants()"
-            (ngModelChange)="onNewParticipantsChange($event)"
-            min="0"
-            [max]="maxParticipants"
-          />
-        } @else {
-          <span>Sorry, no more places available!</span>
-          <button class="secondary outline" (click)="onNewParticipantsChange(0)">Reset</button>
-        }
-        <button [disabled]="canNotBook()" (click)="onBookClick()">Book now for {{ bookingAmount() | currency }}!</button>
-        {{ booked() ? 'Booked!' : '' }}
-      </footer>
-    </article>
+        </footer>
+      </article>
+    }
   `,
-    styles: `
-      .draft {
-        color: violet;
-        font-style: italic;
-      }
-      .published {
-        color: limegreen;
-      }
-      .confirmed {
-        color: green;
-      }
-      .sold_out {
-        color: green;
-        font-style: italic;
-      }
-      .done {
-        color: orange;
-        font-style: italic;
-      }
-      .cancelled {
-        color: red;
-        font-style: italic;
-      }`,
-    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class BookingsPage {
-  readonly activity: Activity = {
-    name: 'Paddle surf',
-    location: 'Lake Leman at Lausanne',
-    price: 125,
-    date: new Date(2025, 7, 15),
-    minParticipants: 5,
-    maxParticipants: 9,
-    status: 'published',
-    id: 1,
-    slug: 'paddle-surf',
-    duration: 2,
-    userId: 1,
-  };
-  readonly alreadyParticipants = 3;
-  readonly maxParticipants = this.activity.maxParticipants - this.alreadyParticipants;
+  slug = input<string>();
+  activity = computed(() => ACTIVITIES.find((a) => a.slug === this.slug()) || NULL_ACTIVITY);
 
-  readonly participants = signal<{ id:number }[]>([{ id:1 }, { id:2 }, { id:3 }]);
+  alreadyParticipants = computed(() => Math.floor(Math.random() * this.activity().maxParticipants));
+  maxParticipants = computed(() => this.activity().maxParticipants - this.alreadyParticipants());
+  isBookable = computed(() => ['published', 'confirmed'].includes(this.activity().status));
 
-  readonly totalParticipants = computed(() => this.alreadyParticipants + this.newParticipants());
-  readonly remainingPlaces = computed(() => this.activity.maxParticipants - this.totalParticipants());
+  newParticipants = signal(0);
+  booked = signal(false);
+  participants = signal<{ id:number }[]>([]);
 
-  readonly canNotBook = computed(() => this.booked() || this.newParticipants() === 0);
-  readonly bookingAmount = computed(() => this.newParticipants() * this.activity.price);
+  totalParticipants = computed(() => this.alreadyParticipants() + this.newParticipants());
+  remainingPlaces = computed(() => this.activity().maxParticipants - this.totalParticipants());
+  bookingAmount = computed(() => this.newParticipants() * this.activity().price);
 
-  readonly newParticipants = signal(0);
-  readonly booked = signal(false);
+  bookedMessage = computed(() => {
+    if(this.booked()) return `Booked USD ${this.bookingAmount()}`;
+    return '';
+  });
 
   constructor() {
-    effect(() => {
-      const totalParticipants = this.totalParticipants();
-      const activity = this.activity;
-      if (totalParticipants >= activity.maxParticipants) {
-        activity.status = 'sold-out';
-      } else if (totalParticipants >= activity.minParticipants) {
-        activity.status = 'confirmed';
-      } else {
-        activity.status = 'published';
+    effect(
+      () => {
+        this.participants.update((participants) => {
+          participants.splice(0, participants.length);
+          for( let i = 0; i < this.totalParticipants(); i++) {
+            participants.push({ id: participants.length + 1});
+          }
+          return participants;
+        });
+      },
+      {
+        allowSignalWrites: true,
       }
+    );
+    effect(() => {
+      if(!this.isBookable()) {
+        return;
+      }
+      const totalParticipants = this.totalParticipants();
+      const activity = this.activity();
+      let newStatus = activity.status;
+      if (totalParticipants >= activity.maxParticipants) {
+        newStatus = 'sold-out';
+      } else if (totalParticipants >= activity.minParticipants) {
+        newStatus = 'confirmed';
+      } else {
+        newStatus = 'published';
+      }
+      activity.status = newStatus;
     });
   }
 
-  onNewParticipantsChange(newParticipants:number) {
+  onNewParticipantsChange(newParticipants: number) {
+    if(newParticipants>this.maxParticipants()) {
+      newParticipants = this.maxParticipants();
+    }
     this.newParticipants.set(newParticipants);
-    this.participants.update((participants) => {
-      participants = participants.slice(0, this.alreadyParticipants);
-      for( let i = 0; i < newParticipants; i++) {
-        participants.push({ id: participants.length + 1});
-      }
-      return participants;
-    });
   }
 
   onBookClick() {
